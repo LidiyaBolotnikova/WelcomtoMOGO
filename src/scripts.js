a2d722a3e5774a623b6bfae9275aa85b2033ab49
libo0619 <lidiyafrdz@gmail.com>
2019-07-05 20:17:04 +0400

    Redact mistakes of all pages2

--- src/scripts.js
+++ src/scripts.js
@@ -5,6 +5,32 @@ $(document).ready(function() {
     { const ps = new PerfectScrollbar($(this)[0]);
         });
 
+    var leftUIEl= $(document.querySelector(".carousel-arrow-left"));
+    var rightUiEl = $(document.querySelector(".carousel-arrow-right"));
+
+    var elementsList = $(".carousel-content");
+    var elemWidth = 1046,
+        elemOffset = 20,
+        elemTotalWidth = 1046,
+        currentLeftValue = 0,
+        elementsCount = elementsList.find(".carousel-content__child").length,
+        listWidth=(elementsCount-1) * elemWidth,
+        maximumOffset = 0;
+
+    leftUIEl.click(function() {
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += listWidth;
+            elementsList.animate({ marginLeft : currentLeftValue + "px"}, 500);
+        }
+    });
+
+    rightUiEl.click(function(){
+        if (Math.abs(currentLeftValue) < listWidth) {
+            currentLeftValue -= listWidth;
+            elementsList.animate({ marginLeft : currentLeftValue + "px"}, 500);
+        }
+    });
+
 });
 
 function openReview(){
